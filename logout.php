6b7417bfaf73dc3f943400876b6f568f88173f22
Saiqul Kencoz <mbekqwerty@gmail.com>
2021-06-12 14:31:53 +0700

    commit 3 - button logout

---
+++ logout.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+session_unset();
+
+echo "
+        <script>
+            alert('Logout Berhasil');
+            window.location.href = '/rental-kamera';
+        </script>
+    ";
+?>
